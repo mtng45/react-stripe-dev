ae49159cb9c9ce71581ba72991d46e8400dd900e
matsunagakenta <k_matsunaga@waku-2.com>
2019-04-19 15:49:42 +0900

    done step 3

--- src/CheckoutForm.js
+++ src/CheckoutForm.js
@@ -1,19 +1,29 @@
-import React, { Component } from 'react'
-import { CardElement, indectStripe } from 'react-stripe-elements'
+import React, {Component} from 'react'
+import {CardElement, injectStripe} from 'react-stripe-elements'
 
 class CheckoutForm extends Component {
   constructor(props) {
     super(props)
+    this.state = { complete: false }
     this.submit = this.submit.bind(this)
   }
 
   async submit(ev) {
-    // User clicked submit
+    // ストライププロップで createToken を呼び出してカード情報をトークン化
+    let { token } = await this.props.stripe.createToken({ name: 'Name' })
+    let response = await fetch('/charge', {
+      method: 'POST',
+      headers: {'Content-Type': 'text/plain'},
+      body: token.id
+    });
+  
+    if (response.ok) this.setState({ complete: true })
   }
 
   render() {
+    if (this.state.complete) return <h1>Purchase Complete</h1>
     return (
-      <div className="checkout">
+      <div className='checkout'>
         <p>Would you like to complete the purchase?</p>
         <CardElement />
         <button onClick={this.submit}>Send</button>
@@ -22,4 +32,4 @@ class CheckoutForm extends Component {
   }
 }
 
-export default indectStripe(CheckoutForm)
+export default injectStripe(CheckoutForm)
